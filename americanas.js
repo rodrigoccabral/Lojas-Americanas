0ef791c1d4ac2424cbccfb92a7a18a1a0c2481e1
rodrigo <rodrigocabral.122@gmail.com>
2020-02-16 13:38:09 -0300

    commit

--- americanas.js
+++ americanas.js
@@ -65,3 +65,19 @@ return curtop;
 }
 
 //-------FIM DO STICKY JAVASCRIPT-----------//
+
+
+
+//-----------------TREEVIEW DO SIDEBAR-------------//
+
+var toggler = document.getElementsByClassName("caret");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+
+  });
+}
+//-----------------FIM DO TREEVIEW DO SIDEBAR-------//
